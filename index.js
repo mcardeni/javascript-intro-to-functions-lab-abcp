1345c39132cef7c6006bc6a84931b88e3b8f0dbb
cardenio@udel.edu <cardenio@udel.edu>
2018-01-22 01:35:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   var words = string
   if (words.toLowerCase() === words)
   return "I can't hear you!"
@@ -19,4 +20,9 @@ function sayHiToGrandma(string) {
   return "YES INDEED!"
   if(words === "I love you, Grandma.")
   return "I love you, too."
+=======
+  if (string.tolowercase() === lowercase)
+  return "I can't here you"
+  if (string.tou)
+>>>>>>> d2ed544fa7e2e624a5f814bf95a87e043a3eddce
 }
